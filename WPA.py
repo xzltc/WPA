45c5004bef9e89879956ddc3776e794863fb37f8
xzltc <xzhltc@gmail.com>
2020-11-15 09:23:00 +0800

    加入围攻及强者生存，优化迭代逻辑

--- WPA.py
+++ WPA.py
@@ -3,23 +3,22 @@
 # @Author: XZL
 # @File : WPA.py
 # @Software: PyCharm
-import math as m
 import numpy as np
 import function
 import heapq
 
-wolf_num = 20  # 狼群数量
-max_iteration = 300  # 最大迭代次数
 α = 4  # 探狼比例因子
 β = 6  # 狼群更新比例因子
-T_max = 30  # 最大游走次数
 ω = 500  # 距离判定因子
 S = 1000  # 步长因子
+T_max = 30  # 最大游走次数
+wolf_num = 50  # 狼群数量
+max_iteration = 300  # 最大迭代次数
 number_T = np.random.randint(wolf_num / (α + 1), wolf_num / α)  # 探狼数量
 
-# 带寻优变量的取值范围
-min_d = -5
-max_d = 5
+# 待寻优变量的取值范围
+min_d = -10
+max_d = 10
 
 # 探狼探寻方向
 h_max = 15
@@ -29,44 +28,43 @@ step_a = (max_d - min_d) / S  # 探狼游走步长
 step_b = 2 * step_a  # 猛狼奔袭步长
 step_c = step_a / 2  # 围攻步长
 
-# 维度(每只人工狼的参数)
-Xsize = 2
+Xsize = 6  # 维度(方程的参数维度)
 
-# 初始化狼群以及计算猎物浓度
+# 初始化狼群以及计算猎物浓度(取值范围均匀分布)
 wolf_colony_X = np.random.uniform(min_d, max_d, (wolf_num, Xsize))
-wolf_colony_Y = function.func2(wolf_colony_X)
 
-# 初始化头狼
-L_Wolf_V = np.min(wolf_colony_Y)
-L_Wolf_index = np.argmin(wolf_colony_Y)
-L_Wolf_X = wolf_colony_X[L_Wolf_index]
-L_Wolf = [L_Wolf_V, L_Wolf_index]
-print("头狼:%f" % L_Wolf_V)
-# 初始化探狼
-T_Wolf_V = heapq.nsmallest(number_T, wolf_colony_Y.copy())
-T_Wolf = function.find_index(T_Wolf_V, wolf_colony_Y.tolist())  # 0:群中下标 1:解
-# 初始化猛狼
-M_Wolf_V = heapq.nlargest(wolf_num - number_T - 1, wolf_colony_Y.copy())
-M_Wolf = function.find_index(M_Wolf_V, wolf_colony_Y.tolist())  # 0:群中下标 1:解
-
-# 发起召唤的狼
-
-
-# 迭代 执行几次
+# 迭代 每次迭代都是完整一次狼群行为
 for it in range(0, max_iteration):
+    print("------开始第 %d 次迭代--------" % (it + 1))
 
-    for i in range(0, T_Wolf.shape[0]):  # 从第一头探狼开始
-        H = np.random.randint(h_min, h_max)  # 当前探狼每次尝试的方向
-        single_T_Wolf = wolf_colony_X[int(T_Wolf[i][0])]  # 变量
+    wolf_colony_V = function.func2(wolf_colony_X)
+    # 初始化头狼
+    L_Wolf_V = np.min(wolf_colony_V)
+    L_Wolf_index = np.argmin(wolf_colony_V)
+    L_Wolf_X = wolf_colony_X[L_Wolf_index]
+    L_Wolf = [L_Wolf_V, L_Wolf_index]
+    print(" > 初始化头狼:%f" % L_Wolf_V)
+    # 初始化探狼
+    T_Wolf_V = heapq.nsmallest(number_T, wolf_colony_V.copy())
+    T_Wolf = function.find_index(T_Wolf_V, wolf_colony_V.tolist())  # 0:群中下标 1:解
+    # 初始化猛狼
+    M_Wolf_V = heapq.nlargest(wolf_num - number_T - 1, wolf_colony_V.copy())
+    M_Wolf = function.find_index(M_Wolf_V, wolf_colony_V.tolist())  # 0:群中下标 1:解
+
+    # 探狼开始游走
+    for i in range(0, T_Wolf.shape[0]):
+        H = np.random.randint(h_min, h_max)  # 尝试的方向
+        single_T_Wolf = wolf_colony_X[int(T_Wolf[i][0])]  # 当前探狼
         optimum_value = T_Wolf[i][1]  # 解
         optimum_position = single_T_Wolf.copy()
         find = False
 
-        # 探狼游走行为
+        # 探狼游走行为 一旦探狼发现目标 -> 开始召唤
         for t in range(0, T_max):
 
+            # 在初始位置朝H个方向试探，直到找到一个优解
             for p in range(1, H + 1):
-                single_T_Wolf_trial = single_T_Wolf.copy()  # 每次在初始位置朝H个方向试探，直到找到一个优解
+                single_T_Wolf_trial = single_T_Wolf.copy()
                 # single_T_Wolf_trial = single_T_Wolf_trial + m.cos(m.pi * p / H) * step_a
                 single_T_Wolf_trial = single_T_Wolf_trial + np.random.uniform(-1, 2,
                                                                               (single_T_Wolf_trial.shape[0],)) * step_a
@@ -81,23 +79,30 @@ for it in range(0, max_iteration):
                     L_Wolf_index = int(T_Wolf[i][0])  # 更新头狼下标
                     wolf_colony_X[L_Wolf_index] = single_T_Wolf_trial  # 更新头狼位置参数
 
+                    T_Wolf = np.delete(T_Wolf, i, axis=0)  # 探狼转为头狼，发起召唤，删除探狼
                     break
 
                 elif optimum_value > single_T_Wolf_V:
                     optimum_value = single_T_Wolf_V
                     optimum_position = single_T_Wolf_trial
+
             else:
                 print("探狼完成第%d游走,未找到猎物" % (t + 1))
+                # 记录上次的最优位置
                 single_T_Wolf = optimum_position
 
             if find is True:
                 break
 
         if find is True:
-            print("探狼发现猎物 %f" % single_T_Wolf_V)
+            print("第%d只探狼发现猎物 %f" % (i, single_T_Wolf_V))
             break
 
-    d_near = (1 / (Xsize * ω)) * Xsize * (max_d - min_d)  # dnear值，判定距离
+        else:
+            # 若游走完成探狼没找到猎物，更新所有游走过程中最优的一次位置
+            wolf_colony_X[int(T_Wolf[i][0])] = optimum_position
+
+    d_near = (1 / ω) * Xsize * (max_d - min_d)  # dnear值，判定距离
 
     # 召唤行为
     surrounded = False
@@ -135,20 +140,47 @@ for it in range(0, max_iteration):
                     break
 
             if d_near > dd:
-                ready += 1  # 围攻就绪态
+                ready += 1  # 围攻就绪态+1
             else:
                 break
 
+        # 所有猛狼是否就位
         if ready == M_Wolf.shape[0]:
-            print("所有猛狼已进入围攻状态")
+            print(" > 所有猛狼已进入围攻状态")
             break
 
     # 围攻行为
-    # for
+    for m in range(0, M_Wolf.shape[0]):
+        s_m_index = int(M_Wolf[m][0])
+        single_M_Wolf = wolf_colony_X[s_m_index]
+        # 发起围攻，计算围攻后位置
+        single_M_Wolf = single_M_Wolf + np.random.uniform(-1, 1) * step_c * np.abs(L_Wolf_X - single_M_Wolf)
+        single_M_Wolf_V = function.func2(single_M_Wolf)[0]
 
-    # 一次结束后，去掉wolf_colony_X中最后的几个狼，生成新的人工狼
-    # 重新分配头狼、探狼、猛狼
+        if L_Wolf_V > single_M_Wolf_V:
+            print(" > 发起围攻!目标更新 原值:%f 现值:%f" % (L_Wolf_V, single_M_Wolf_V))
+            # 头狼变猛狼
+            M_Wolf[m][0] = L_Wolf_index
+            M_Wolf[m][1] = L_Wolf_V
+            wolf_colony_X[L_Wolf_index] = L_Wolf_X
+
+            # 猛狼变头狼
+            L_Wolf_V = single_M_Wolf_V
+            L_Wolf_X = single_M_Wolf
+            L_Wolf_index = s_m_index
+            wolf_colony_X[L_Wolf_index] = single_M_Wolf
+    print(" > 围攻完成")
+
+    # 强者生存行为
+    wolf_colony_V = function.func2(wolf_colony_X)  # 重新计算现在所有狼狩猎的状态
+    eliminate_number = np.random.randint(wolf_num / (2 * β), wolf_num / β)
+    Bad_Wolf_V = heapq.nlargest(eliminate_number, wolf_colony_V.copy())
+    Bad_Wolf = function.find_index(Bad_Wolf_V, wolf_colony_V.tolist())
+    # 生成新的人工狼
+    new_wolf = np.random.uniform(min_d, max_d, (eliminate_number, Xsize))
+    # 淘汰最弱的狼
+    for n in range(0, len(Bad_Wolf_V)):
+        wolf_colony_X[int(Bad_Wolf[n][0])] = new_wolf[n]
 
     print(wolf_colony_X[L_Wolf_index])
-    print(L_Wolf_V)
-print("111")
+    print(" > 头狼:%f" % L_Wolf_V)
--- function.py
+++ function.py
@@ -22,10 +22,28 @@ def func2(X):
     for i in range(0, X.shape[0]):
         x1 = X[i][0]
         x2 = X[i][1]
-        res[i] = 0.26 * (pow(x1, 2) + pow(x2, 2)) + 0.5 * x1 * x2
+        # res[i] = 0.26 * (pow(x1, 2) + pow(x2, 2)) + 0.5 * x1 * x2
         # res[i] = -m.cos(x1) * m.cos(x2) * m.exp(-pow((x1 - m.pi), 2) - pow((x2 - m.pi), 2))
         # res[i] = 4 * pow(x1, 2) - 2.1 * pow(x1, 4) + (1 / 3) * pow(x1, 6) + x1 * x2 - 4 * pow(x2, 2) + 4 * pow(x2, 4)
         # res[i] = x1 + x2
+        res[i] = pow((x1 + 2 * x2 - 7), 2) + pow((2 * x1 + x2 - 5), 2)
+    return res
+
+
+def func6(X):
+    if X.ndim == 1:
+        X = np.reshape(X, (1,) + X.shape)
+    res = np.zeros((X.shape[0]))
+    for i in range(0, X.shape[0]):
+        x1 = X[i][0]
+        x2 = X[i][1]
+        x3 = X[i][2]
+        x4 = X[i][3]
+        x5 = X[i][4]
+        x6 = X[i][5]
+        res[i] = (pow(x1 - 1, 2) + pow(x2 - 1, 2) + pow(x3 - 1, 2) + pow(x4 - 1, 2) + pow(x5 - 1, 2) + pow(x6 - 1, 2)) \
+                 - (x2 * x1 + x3 * x2 + x4 * x3 + x5 * x4 + x6 * x5)
+
     return res
 
 
